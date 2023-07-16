30dc73d9707e0df27d7d4d324947bdbf2b5d7eb9
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-07-16 16:02:23 -0400

    added routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,8 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import Routers from './routes/Routers';
 
 function App() {
-  const [backendData, setBackendData] = useState([{}]);
-  useEffect(() => {
-    const fetchData = async () => {
-    const result = await fetch("/api")
-    const data = await result.json()
-    setBackendData(data)
-   }
-   fetchData()
-    }, [])
-
-    const submitPost = async () => {
-      const myData = {
-        title: "postFour"
-      }
-
-      const result = await fetch("http://localhost:4000/api", {
-        method: "POST",
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(myData)
-      })
-
-      const resultJson = await result.json()
-      setBackendData(prev => [...prev, resultJson])
-    }
-
-  return (
-    <>
-    <button onClick={submitPost}>Submit a post</button>
-     {(typeof backendData.posts === 'undefined') ? (
-      <p>Loading...</p>
-     ): (
-      backendData.posts.map((post, i) => (
-        <p key={i}>{post.title}</p>
-      ))
-     )}
-    </>
-  );
+  <Routers />
 }
 
 export default App;
